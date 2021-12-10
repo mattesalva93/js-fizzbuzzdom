8080880b4c0ad3a754509c498221a2f3736f4650
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-10 15:05:24 +0100

    number boxes generator done

---
+++ myscript.js
@@ -0,0 +1,17 @@
+let scatola = document.getElementById("container"); //Definizione contenitore per div.row
+
+for(let i=1; i<=100; i++){              //Definizine del ciclo da 1 a 100
+
+    if( (i%3==0) && (i%5==0) ){         //Numeri multipli di 3 e 5
+        scatola.innerHTML += '<div class="col-2 m-4 text-center">' + 'FizzBuzz' + '</div>';
+    
+    }else if(i%3==0){                   //Numeri multipli di 3
+        scatola.innerHTML += '<div class="col-2 m-4 text-center">' + 'Fizz' + '</div>';
+    
+    }else if(i%5==0){                   //Numeri multipli di 5
+        scatola.innerHTML += '<div class="col-2 m-4 text-center">' + 'Buzz' + '</div>';
+    
+    }else{                              //Tutti gli altri
+        scatola.innerHTML += '<div class="col-2 m-4 text-center">' + i + '</div>';
+    }
+}

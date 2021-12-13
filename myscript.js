f41b8ae9f8ddfd81a1d689534488427c9535862e
mattesalva93 <salvalaggiomatteo93@gmail.com>
2021-12-13 10:09:34 +0100

    post correzione in classe

--- myscript.js
+++ myscript.js
@@ -4,12 +4,12 @@ for(let i=1; i<=100; i++){              //Definizine del ciclo da 1 a 100
     console.log('iterazione: ', i)
 
     if( (i%3==0) && (i%5==0) ){         //Numeri multipli di 3 e 5
-        scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_fizzbuzz-box">' + 'FizzBuzz' + '</div>';
+        scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_fizzbuzz-box"> FizzBuzz </div>';
     }else if(i%3==0){                   //Numeri multipli di 3
-       scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_fizz-box">' + 'Fizz' + '</div>';
+        scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_fizz-box"> Fizz </div>';
     }else if(i%5==0){                   //Numeri multipli di 5
-        scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_buzz-box">' + 'Buzz' + '</div>';
+        scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_buzz-box"> Buzz </div>';
     }else{                              //Tutti gli altri
         scatola.innerHTML += '<div class="col-1 p-4 m-1 text-center ms_regular-box">' + i + '</div>';
     }
-}
+}1
